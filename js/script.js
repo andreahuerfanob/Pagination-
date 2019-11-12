dcde5926fa87263cf877bbc4e886fee317c9a98a
andreahuerfanob <andreahuerfano@live.com>
2019-11-12 16:01:41 -0500

    u2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 /***
+ * https://www.digitalocean.com/community/tutorials/introduction-to-the-dom
 FSJS project 2 - List Filter and Pagination
 ***/
 /*** 
    Add global variables that store the DOM elements needed to reference and/or manipulate. 
 ***/
 // Variable to store nodeList of DOM elements:
-const studentList = document.querySelector(".student-list");
-const items = document.querySelectorAll('.student-item');
+const studentList = document.getElementsByClassName('student-item');
 // Variable to store the number of items to show at any given time
 const perPage = 10;
 // var to start page number with:
@@ -17,7 +17,7 @@ const startPageNumber = 1;
 ***/
 const showPage = (list, page) => {
   // variables to retrieve the index for 1st & last student on a particular page.
-  const startIndex = page * perPage - perPage;
+  const startIndex = (page * perPage)- perPage;
   const endIndex = page * perPage;
   // loop over list
   // if index >= 1st index = show
@@ -36,29 +36,32 @@ const showPage = (list, page) => {
 ***/
 const appendPageLinks = // //single list parameter to represent the actual list of students:
 (list) => {
-  showPage(items, 1); // showPage function 
-  const totalPages = Math.ceil(list.length / perPage);  // round up and store in totalPages
 
+  const totalPages = Math.ceil(list.length / perPage);  // round up and store in totalPages
 // elements for pagination
 const div = document.createElement('div');
 div.className = 'pagination';
 const pageDiv = document.querySelector('.page');
 pageDiv.appendChild(div);
 const ulLinks = document.createElement('ul');
-   div.appendChild(ulLinks);
-  
-/// here starts the shitshow: 
+div.appendChild(ulLinks);
+
+/// For loop for li: 
    for (let i = 1; i <= totalPages; i++) {
     const li = document.createElement('li');
     ulPagLinks.appendChild(li);
     const a = document.createElement('a');
     li.appendChild(a);
+// LI = A elem, href att = #,txt = page # that links show
     a.textContent = i;
     a.href = '#';
+
+//set active class on first pagination link
     const firstA = document.querySelector('a');
     firstA.className = 'active';
     const aList = document.querySelectorAll('a');
-    //Adding an event listener to each link
+
+    //Add event listener to each link
     for (let j = 1; j <= aList.length; j++) {
        a.addEventListener('click', (event) => {
           showPage(studentList, j);
@@ -67,6 +70,10 @@ const ulLinks = document.createElement('ul');
           }
           event.target.className = 'active';
     });
-    }
- }
 }
+   }
+}
+showPage(items, 1); 
+appendPageLinks(studentList);
+
+https://github.com/pixelspencil/list-pagination-and-filtering-th-fsjs/blob/master/js/script.js
